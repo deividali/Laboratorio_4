de98389ff705c2d329e7df15080146ae74612615
deividali <damingan123@gmail.com>
2025-07-14 19:54:44 -0500

    javascript code

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const handlerForm = ()=>{
+    let name = document.getElementById("name").value;
+    let contact = document.getElementById("contact").value;
+    let email = document.getElementById("email").value;
+
+    alert("Su nombre es "+name+"\nMotivo de consulta "+contact+"\nSu correo es "+email);
+
+
+}
